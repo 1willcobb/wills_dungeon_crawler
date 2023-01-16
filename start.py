f9de12f25e2a482e95451ee1cc1558535c2e7f24
1willcobb <cobb.will@gmail.com>
2023-01-16 07:26:20 -0800

    working on getting rooms together and the __main__ file working

--- start.py
+++ start.py
@@ -1,11 +1,11 @@
-from rooms import Rooms
-from monsters import Monsters
 import player
 
 def game_start():
     will = player.Player("will", 32, "dragon")
     will.initialize_rooms()
+    print(will.name)
     
 
 if __name__ == "__main__":
     print("start the game")
+    game_start()
