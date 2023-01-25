dbb537fb7c313ea7e6fe0c05d391a40965c69d8d
1willcobb <cobb.will@gmail.com>
2023-01-25 07:42:00 -0800

    added new monster attack defence

--- monsters.py
+++ monsters.py
@@ -24,4 +24,28 @@ monsters = {
 			'defence': '2',
 			'alive': True
 			},
+  	'1_1': {'name': 'fat lizard',
+			'life': 10,
+			'attack': 2,
+			'defence': 3,
+			'alive': True
+			},
+	'1_2': {'name': 'little baby centar',
+			'life': 2,
+			'attack': 1,
+			'defence':0,
+			'alive': True
+			},
+  '1_3': {'name': 'big baby centar',
+      'life': '5',
+      'attack': '2',
+      'defence': '1',
+      'alive': True
+			},
+  '1_4': {'name': 'big baby centar the momnster',
+      'life': '10',
+			'attack': '4',
+			'defence': '2',
+			'alive': True
+			},
 }
